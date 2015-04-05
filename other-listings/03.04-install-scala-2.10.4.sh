a6c991b884d266efa551b8f3f09912c47f355e3e
mbonaci <mbonaci@gmail.com>
2015-04-05 15:30:13 +0200

    moved things around a bit

--- other-listings/03.04-install-scala-2.10.4.sh
+++ other-listings/03.04-install-scala-2.10.4.sh
@@ -1,9 +1,6 @@
 sudo apt-get remove scala-library scala
 sudo wget www.scala-lang.org/files/archive/scala-2.10.4.deb
 sudo dpkg -i scala-2.10.4.deb
-sudo apt-get update
-sudo apt-get install scala
-wget http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.12.4/sbt.deb
-sudo dpkg -i sbt.deb
-sudo apt-get update
-sudo apt-get install sbt
+
+wget https://dl.bintray.com/sbt/debian/sbt-0.13.8.deb
+sudo dpkg -i sbt-0.13.8.deb
